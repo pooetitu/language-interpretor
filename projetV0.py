546e053a066fc41ec226e85780590043ba666c3a
pooetitu <pooetitu@gmail.com>
2021-06-09 23:51:59 +0200

    Add function param parse

--- projetV0.py
+++ projetV0.py
@@ -5,8 +5,8 @@ reserved = {
    'print' : 'PRINT',
    'while' : 'WHILE',
    'for' : 'FOR',
-   'fonctionValue' : "FONCTION_VALUE",
-   'fonctionVoid' : "FONCTION_VOID",
+   'functionValue' : "FONCTION_VALUE",
+   'functionVoid' : "FONCTION_VOID",
    'return' : 'RETURN'
    }
 
@@ -202,8 +202,9 @@ def p_expression_function_value_call(t):
 def p_error(t):
     print("Syntax error at '%s'" % t.value)
 
-functions={}
-functions_scope={}
+functions_value={}
+functions_void={}
+functions_scope_stack=[]
 names={}
 
 def eval_inst(tree):
@@ -234,10 +235,18 @@ def eval_inst(tree):
         names[tree[1]]+=tree[2]
     elif tree[0] == "minus_equals":
         names[tree[1]]-=tree[2]
-    elif tree[0] == "fonction_void" or tree[0] == "fonction_value":
-        functions[tree[1]] = tree[0]
-    elif tree[0] == "function_call":
-        print("a")
+    elif tree[0] == "function_void":
+        functions_void[tree[1]] = tree
+    elif tree[0] == "function_value":
+        functions_value[tree[1]] = tree
+    elif tree[0] == "function_void_call":
+        functions_scope_stack.append({})
+        load_function_params(tree, functions_void[tree[1]])
+        functions_scope_stack.pop()
+    elif tree[0] == "function_value_call":
+        functions_scope_stack.append({})
+        load_function_params(tree, functions_value[tree[1]])
+        functions_scope_stack.pop()
     elif tree != "empty":
         eval_expr(tree)
 
@@ -268,6 +277,15 @@ def eval_expr(tree):
         return tree
 
 
+def load_function_params(tree, function):
+    param_name = function[2]
+    param = tree[2]
+    while param and param_name:
+        functions_scope_stack[len(functions_scope_stack)-1][param_name[1]] = param[1]
+        if len(param) == 2 or len(param_name) == 2:
+            break
+        param_name = param_name[2]
+        param = param[2]
 
 import ply.yacc as yacc
 parser = yacc.yacc()
@@ -275,7 +293,7 @@ parser = yacc.yacc()
 #s='1+2;x=4 if ;x=x+1;'
 #s='for(i=0;i<4;i++;){print(i*i);}'# boucle for et incrementation
 #s='max=20;count=0;i=0;j=1;while(count<max)then;count++;print(i);tmp=j+i;i=j;j=tmp;end;' # fibonnacci boucle while
-s='fonctionVoid naame(a,b){print(a); print(a+b);}naame(1,2);'
+s='functionVoid naame(a,b){print(a); print(a+b);}naame(1,2);'
 #with open("1.in") as file: # Use file to refer to the file object
 
    #s = file.read()
