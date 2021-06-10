310cf1b0d8ae10f1dfd033e8241a42ed9e3ed4f1
pooetitu <pooetitu@gmail.com>
2021-06-10 15:22:04 +0200

    Implement return stops function execution

--- projetV0.py
+++ projetV0.py
@@ -86,7 +86,7 @@ def p_start(t):
     ''' start : linst'''
     t[0] = ('start',t[1])
     print(t[0])
-    #printTreeGraph(t[0])
+    printTreeGraph(t[0])
     #eval(t[1])
     eval_inst(t[1])
     
@@ -98,6 +98,24 @@ def p_line(t):
     else:
         t[0] = ('bloc',t[1], 'empty')
     
+def p_function_void_linst(t):
+    '''linst_void : linst_void inst_void
+            | inst_void '''
+    if len(t) == 3 :
+        t[0] = ('bloc',t[1], t[2])
+    elif len:
+        t[0] = ('bloc', t[1], 'empty')
+
+def p_function_value_linst(t):
+    '''linst_value : linst_value inst_value
+        | linst_value return_value
+        | return_value
+        | inst_value'''
+    if len(t) == 3:
+        t[0] = ('bloc', t[1], t[2])
+    else:
+        t[0] = ('bloc', t[1], 'empty')
+
 def p_params(t):
     '''params : NAME COMMA params
         | NAME '''
@@ -116,29 +134,38 @@ def p_function_call_params(t):
         t[0] = ('param', t[1])
 
 def p_statement_function_value_definition(t):
-    '''inst : FONCTION_VALUE NAME LPAREN params RPAREN LBRACKET RETURN expression COLON RBRACKET
-        | FONCTION_VALUE NAME LPAREN params RPAREN LBRACKET linst RETURN expression COLON RBRACKET'''
+    '''inst : FONCTION_VALUE NAME LPAREN params RPAREN LBRACKET linst_value RBRACKET
+        | FONCTION_VALUE NAME LPAREN RPAREN LBRACKET linst_value RBRACKET'''
     if len(t) == 10:
-        t[0] = ('function_value', t[2], t[4], "empty", t[8])
+        t[0] = ('function_value', t[2], t[4], t[7])
     else:
-        t[0] = ('function_value', t[2], t[4], t[7], t[9])
-
-def p_statement_function_value_no_param_definition(t):
-    '''inst : FONCTION_VALUE NAME LPAREN RPAREN LBRACKET RETURN expression COLON RBRACKET
-        | FONCTION_VALUE NAME LPAREN RPAREN LBRACKET linst RETURN expression COLON RBRACKET'''
-    if len(t) == 10:
-        t[0] = ('function_value', t[2], "empty", "empty", t[7])
-    else:
-        t[0] = ('function_value', t[2], "empty", t[6], t[8])
+        t[0] = ('function_value', t[2], "empty", t[6])
 
 
 def p_statement_function_void_definition(t):
-    '''inst : FONCTION_VOID NAME LPAREN params RPAREN LBRACKET linst RBRACKET
-        | FONCTION_VOID NAME LPAREN RPAREN LBRACKET linst RBRACKET'''
+    '''inst : FONCTION_VOID NAME LPAREN params RPAREN LBRACKET linst_void RBRACKET
+        | FONCTION_VOID NAME LPAREN RPAREN LBRACKET linst_void RBRACKET'''
     if len(t) == 9:
         t[0] = ('function_void', t[2], t[4], t[7])
     else:
         t[0] = ('function_void', t[2], "empty", t[6])
+
+def p_statement_return_value(t):
+    '''return_value : RETURN expression COLON'''
+    t[0] = ('return', t[2])
+
+def p_statement_inst_value(t):
+    '''inst_value : inst
+    | return_value'''
+    t[0] = t[1]
+
+def p_statement_return_void(t):
+    '''inst_void : RETURN COLON'''
+    t[0] = ('return', 'empty')
+
+def p_statement_inst_void(t):
+    '''inst_void : inst'''
+    t[0] = t[1]
 
 def p_statement_if(t):
     'inst : IF LPAREN expression RPAREN LBRACKET linst RBRACKET'
@@ -168,7 +195,6 @@ def p_statement_incr(t):
     'inst : NAME PLUS PLUS COLON'
     t[0] = ('incr', t[1])
 
-    
 def p_statement_decr(t):
     'inst : NAME MINUS MINUS COLON'
     t[0] = ('decr', t[1])
@@ -247,7 +273,10 @@ def eval_inst(tree):
     print("evalInst de " + str(tree)) 
     if tree[0] == "bloc":
         eval_inst(tree[1])
-        eval_inst(tree[2])
+        if not (len(functions_scope_stack) > 0 and 'return' in functions_scope_stack[len(functions_scope_stack)-1]):
+            eval_inst(tree[2])
+    elif tree[0] == "return":
+        get_variable_reference("return")["return"] = None if tree[1] == "empty" else eval_expr(tree[1])
     elif tree[0] == "print":
         print(eval_expr(tree[1]))
     elif tree[0] == "print_string":
@@ -255,16 +284,22 @@ def eval_inst(tree):
     elif tree[0] == "assign":
         get_variable_reference(tree[1])[tree[1]]=eval_expr(tree[2])
     elif tree[0] == "if":
+        functions_scope_stack.append({})
         if eval_expr(tree[1]):
             eval_inst(tree[2])
+        functions_scope_stack.pop()
     elif tree[0] == "while":
+        functions_scope_stack.append({})
         while eval_expr(tree[1]):
             eval_inst(tree[2])
+        functions_scope_stack.pop()
     elif tree[0] == "for":
+        functions_scope_stack.append({})
         eval_inst(tree[1])
         while eval_expr(tree[2]):
             eval_inst(tree[4])
             eval_inst(tree[3])
+        functions_scope_stack.pop()
     elif tree[0] == "incr":
         get_variable_reference(tree[1])[tree[1]]+=1
     elif tree[0] == "decr":
@@ -310,7 +345,7 @@ def eval_expr(tree):
             functions_scope_stack.append({})
             load_function_params(tree, functions_value[tree[1]])
             eval_inst(functions_value[tree[1]][3])
-            return_value = eval_expr(functions_value[tree[1]][4])
+            return_value = get_variable_reference("return")["return"]
             functions_scope_stack.pop()
             return return_value
     elif type(tree) == str:
@@ -342,7 +377,7 @@ import ply.yacc as yacc
 parser = yacc.yacc()
 
 #s='1+2;x=4 if ;x=x+1;'
-s='printString("Hello world");' # print string
+#s='printString("Hello world");' # print string
 #s='for(i=0;i<4;i++;){print(i*i);}'# boucle for et incrementation
 #s='max=20;count=0;i=0;j=1;while(count<max)then;count++;print(i);tmp=j+i;i=j;j=tmp;end;' # fibonnacci boucle while
 #s='functionVoid voidFunction(a,b){print(a); print(a+b);}voidFunction(1,2);' # void function with params
@@ -351,7 +386,8 @@ s='printString("Hello world");' # print string
 #s='functionValue noParamValueFunction(){return 10;}print(noParamValueFunction());' # value function without params
 #s='functionVoid scoppedVariable(){a=1;}print(a);' # can't access to functions scope variable finish with error
 #s='functionVoid globalVariable(){print(a);}a=1;globalVariable();' # void function without params finish with error
-
+s='functionVoid returnStop(){a=1; print(1); return; print(777);}returnStop();' # void function return stops function
+#s='functionValue returnStop(){a=1;print(1);return a+1;print(777);}print(returnStop());' # value function return stops function
 
 #with open("1.in") as file: # Use file to refer to the file object
 
